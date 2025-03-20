f51a101c4dc2261472514b35b8197ce5d3fba363
Franklin Doane <doanefranklin89@gmail.com>
2025-03-20 13:15:04 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// store selected tab
+let strSelectedTabId = "divTab1";
+
+// get all tabs
+let arrTabs = document.querySelectorAll('.tab');
+
+// false if any tab has been clicked
+let boolFirstClick = true;
+
+// add on click to tabs
+arrTabs.forEach(objDivTab => {
+
+    objDivTab.addEventListener('click', function() {
+
+        // check if already selected
+        if (this.classList.contains("tab-selected")) {
+            return;
+        }
+
+        // de-select current selected 
+        document.querySelector(`#${strSelectedTabId}`).classList.remove("tab-selected");
+
+        // add selected class to current
+        this.classList.add("tab-selected");
+
+        // set new selected tab
+        strSelectedTabId = this.id;
+
+        // scroll screen if at the top and first click
+        // set first click to false
+        if (boolFirstClick && window.scrollY == 0) {
+            document.querySelector('#divTabBlock').scrollIntoView();
+            boolFirstClick = false;
+        }
+        else if (boolFirstClick) {
+            boolFirstClick = false;
+        }
+
+    });
+    
+});
